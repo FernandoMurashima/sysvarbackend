dbe3bdc16cb19dc7d2473411400481fc4be15bf4
FernandoMurashima <147065624+FernandoMurashima@users.noreply.github.com>
2026-08-31 17:35:28 -0300

    fix: registrar viewset de NF-e XML com estoque por SKU

--- fiscal/views/__init__.py
+++ fiscal/views/__init__.py
@@ -1,5 +1,6 @@
 from .cfop import CfopViewSet
-from .nota_fiscal_entrada import NotaFiscalEntradaItemViewSet, NotaFiscalEntradaViewSet
+from .nota_fiscal_entrada import NotaFiscalEntradaItemViewSet
+from .nota_fiscal_entrada_sku import NotaFiscalEntradaViewSet
 from .nota_fiscal_saida import NotaFiscalSaidaItemViewSet, NotaFiscalSaidaViewSet
 from .tributacao import RegraTributariaViewSet, TributoViewSet
 from .venda_pdv import NFCeViewSet, VendaDevolucaoViewSet, VendaPdvViewSet
